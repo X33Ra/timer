fc8062b491b1783d4f24f1a7d0e1308416cd2c77
MrChan <mrkcwayne@gmail.com>
2023-06-12 11:07:34 -0600

    added timer's frame structure

---
+++ timer1.js
@@ -0,0 +1,10 @@
+// Implement an alarm clock / timer
+// which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
+// EX:
+//This will make it beep at:
+
+// 3 seconds
+// 5 seconds
+// 9 seconds
+// 10 seconds
+// 15 seconds
