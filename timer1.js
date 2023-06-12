ad517b05d664b3f96944b874772f29a2cd8db8d0
MrChan <mrkcwayne@gmail.com>
2023-06-12 11:11:27 -0600

    added a function that beeps and a function that says your alarm is added

--- timer1.js
+++ timer1.js
@@ -8,3 +8,20 @@
 // 9 seconds
 // 10 seconds
 // 15 seconds
+
+const args = process.argv.slice(2);
+
+const scheduleAlarm = function(time) {
+  setTimeout(() => {
+    console.log('Beep!');
+    process.stdout.write('\x07'); // Beep sound
+  }, time * 1000);
+};
+
+for (const arg of args) {
+  const time = parseInt(arg, 10);
+  if (!isNaN(time) && time > 0) {
+    scheduleAlarm(time);
+    console.log(`Alarm scheduled in ${time} seconds`);
+  }
+}
